e504b1d18ced47c9982451fa2fd7197ceda4559e
frillic <69489498+crowe828@users.noreply.github.com>
2020-11-12 13:30:09 -0500

    added header and navbar from material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="App">
+        <Header />
+        <Nav />
       </div>
     );
   }
