4f0fd92ea432d415f41aa70a4f06f0ed2496e61f
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-11-14 17:22:59 -0500

    added state to be in App instead of Main as we are only going to be using one state in this app

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,48 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Search from "../components/Search";
 import AnimeCard from "../components/AnimeCard/index";
-import API from "../utils/API";
 
-class Main extends Component {
-  state = {
-    results: [],
-    search: "",
-    result: {},
-  };
-
-  componentDidMount() {
-    this.animeInfo("My hero");
-  }
-
-  animeInfo = (query) => {
-    API.getAnime(query)
-      .then((res) => this.setState({ ...this.state, results: res.data.data }))
-      .catch((err) => console.log(err));
-  };
-
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.search);
-    this.animeInfo(this.state.search);
-  };
-
-  render() {
+function Main(props) {
     return (
       <div>
         <Search
-          handleInputChange={this.handleInputChange}
-          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={props.handleInputChange}
+          handleFormSubmit={props.handleFormSubmit}
         />
-        <AnimeCard results={this.state.results} />
+        <AnimeCard results={props.state.results} />
       </div>
     );
-  }
+
 }
 
 export default Main;
