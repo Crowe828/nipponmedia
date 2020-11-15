bc67045b637b32ba224402a9e47e6a8116b95cf5
frillic <69489498+crowe828@users.noreply.github.com>
2020-11-15 13:53:13 -0500

    details page now works

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-export default function Details() {
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { response: null };
+  }
 
-  const url = "https://kitsu.io/api/edge/anime" + window.location.pathname;
+  componentDidMount() {
+    axios
+      .get("https://kitsu.io/api/edge/anime" + window.location.pathname)
+      .then((response) => {
+        console.log(response.data.data);
+        this.setState({ response });
+      });
+  }
 
-  const details = axios.get(url);
-
-  console.log(details);
-
-  return (<div>This is the details page</div>);
+  render() {
+    if (this.state.response == null) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <main>
+          <div>Title: {this.state.response.data.data.attributes.titles.en}</div>
+          <div>
+            Japanese Title:{" "}
+            {this.state.response.data.data.attributes.titles.ja_jp}
+          </div>
+          <img
+            src={this.state.response.data.data.attributes.posterImage.large}
+            alt={this.state.response.data.data.attributes.titles.en}
+          />
+          <div>
+            Synopsis: <br />
+            {this.state.response.data.data.attributes.synopsis}
+          </div>
+        </main>
+      );
+    }
+  }
 }
+
+export default Details;
