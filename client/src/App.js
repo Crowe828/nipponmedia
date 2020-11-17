a069c20b3ebb673647a34f5c719b180054fb1c9a
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-11-17 15:05:52 -0500

    changed app into a functional component so we can add a login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
@@ -11,91 +11,89 @@ import Footer from "./components/Footer";
 import "./App.css";
 import API from "../src/utils/API";
 
-class App extends Component {
+function App() {
   // state for data that comes in for the API call and the search term in the search bar
-  state = {
-    results: [],
-    search: "",
-  };
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState("");
+
   // on mount, state will show popular animes
-  componentDidMount() {
-    this.getDefault();
-  }
+  useEffect(() => {
+    getDefault();
+  }, []);
+
   // function that sets state to popular animes
-  getDefault = () => {
+  function getDefault() {
     API.defaultData()
-      .then((res) => this.setState({ ...this.state, results: res.data.data }))
+      .then((res) => {
+        setResults(res.data.data);
+      })
       .catch((err) => console.log(err));
-  };
+  }
   // function that will show the anime / manga that you searched and set the state
-  getInfo = (query, category) => {
+  function getInfo(query, category) {
     console.log(query, category);
     API.getData(query, category)
-      .then((res) => this.setState({ ...this.state, results: res.data.data }))
+      .then((res) => setResults(res.data.data))
       .catch((err) => console.log(err));
-  };
+  }
   // changes search from id="search" on change and sets the state of it to the current search
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+  function handleInputChange(event) {
+    setSearch(event.target.value);
+  }
   // handles the submit to use the API call dependant on search term and type of anime/manga in category.
   //passing type through Search component from state
-  handleFormSubmit = (event, category) => {
+  function handleFormSubmit(event, category) {
     event.preventDefault();
-    this.getInfo(this.state.search, category);
-  };
-
-  render() {
-    return (
-      <div
-        className="App"
-        style={{
-          position: "relative",
-          minHeight: "100vh",
-        }}
-      >
-        <Router>
-          <div style={{ paddingBottom: "50px" }}>
-            <Header />
-            <Nav />
-            <Switch>
-              <Route exact path="/">
-                <Main
-                  handleFormSubmit={this.handleFormSubmit}
-                  handleInputChange={this.handleInputChange}
-                  handleFormSubmitManga={this.handleFormSubmitManga}
-                  state={this.state}
-                />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/login">
-                <Login />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/signup">
-                <Signup />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/profile">
-                <Profile />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/:type/:id">
-                <Details state={this.state} />
-              </Route>
-            </Switch>
-          </div>
-          <Footer />
-        </Router>
-      </div>
-    );
+    getInfo(search, category);
   }
+
+  return (
+    <div
+      className="App"
+      style={{
+        position: "relative",
+        minHeight: "100vh",
+      }}
+    >
+      <Router>
+        <div style={{ paddingBottom: "50px" }}>
+          <Header />
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <Main
+                handleFormSubmit={handleFormSubmit}
+                handleInputChange={handleInputChange}
+                results={results}
+                search={search}
+              />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/:type/:id">
+              <Details results={results} />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -57,7 +57,10 @@ const styles = () => ({
 class Details extends Component {
   constructor(props) {
     super(props);
-    this.state = { response: null };
+    this.state = {
+      response: null,
+      type: null,
+    };
   }
 
   //this pulls the type and id of the anime from the App.js state
@@ -66,16 +69,15 @@ class Details extends Component {
       .get("https://kitsu.io/api/edge/" + window.location.pathname)
       .then((response) => {
         console.log(response.data.data);
-        this.setState({ response });
+        this.setState({ response: response, type: response.data.data.type });
+        console.log("type: " + this.state.type);
       });
   }
 
-// will render anime detail, manga detail, or nothing depending on state
+  // will render anime detail, manga detail, or nothing depending on state
   render() {
     const { classes } = this.props;
-    if (this.state.response == null) {
-      return (<div>Loading...</div>);
-    } else {
+    if (this.state.type === "manga") {
       return (
         <main className={classes.main}>
           <div className={classes.center}>
@@ -111,9 +113,102 @@ class Details extends Component {
                   </Typography>
                 </Grid>
                 <Grid className={classes.card} item xs={5}>
+                  {this.state.response.data.data.attributes.endDate == null ? (
+                    <Typography component={"span"}>End Date: TBD</Typography>
+                  ) : (
+                    <Typography component={"span"}>
+                      End Date:{" "}
+                      {this.state.response.data.data.attributes.endDate}
+                    </Typography>
+                  )}
+                </Grid>
+                <Grid className={classes.cardSmall} item xs={2}>
                   <Typography>
-                    End date: {this.state.response.data.data.attributes.endDate}
+                    Status: {this.state.response.data.data.attributes.status}
                   </Typography>
+                </Grid>
+                <Grid className={classes.cardSmall} item xs={2}>
+                  <Typography>
+                    Reader Rating:{" "}
+                    {this.state.response.data.data.attributes.averageRating}/100
+                  </Typography>
+                </Grid>
+                <Grid className={classes.cardSmall} item xs={2}>
+                  <Typography>
+                    Rank among Mangas:{" "}
+                    {this.state.response.data.data.attributes.popularityRank}
+                  </Typography>
+                </Grid>
+                <Grid className={classes.cardSmall} item xs={2}>
+                  <Typography>
+                    NSFW: {this.state.response.data.data.attributes.nsfw}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.btnGroup}>
+            <Button variant="contained" color="primary">
+              Watching
+            </Button>
+            <Button variant="contained" color="secondary">
+              Watched
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="medium"
+              startIcon={<SaveIcon />}
+            >
+              Save as Favorite
+            </Button>
+          </div>
+        </main>
+      );
+    } else if (this.state.type === "anime") {
+      return (
+        <main className={classes.main}>
+          <div className={classes.center}>
+            {this.state.response.data.data.attributes.titles.en}
+          </div>
+          <div className={classes.center}>
+            {" "}
+            {this.state.response.data.data.attributes.titles.ja_jp}
+          </div>
+          <div className={classes.center}>
+            Age Guide: {this.state.response.data.data.attributes.ageRating}{" "}
+          </div>
+          <div className={classes.center}>
+            <img
+              className={classes.imageSize}
+              src={this.state.response.data.data.attributes.posterImage.large}
+              alt={this.state.response.data.data.attributes.titles.en}
+            />
+          </div>
+          <Card className={classes.textCenter}>
+            <CardContent>
+              <Grid className={classes.spaceBetween} container spacing={3}>
+                <Grid item xs={12}>
+                  <Typography>
+                    Synopsis: <br />
+                    {this.state.response.data.data.attributes.synopsis}
+                  </Typography>
+                </Grid>
+                <Grid className={classes.card} item xs={5}>
+                  <Typography>
+                    Start date:{" "}
+                    {this.state.response.data.data.attributes.startDate}{" "}
+                  </Typography>
+                </Grid>
+                <Grid className={classes.card} item xs={5}>
+                  {this.state.response.data.data.attributes.endDate == null ? (
+                    <Typography component={"span"}>End Date: TBD</Typography>
+                  ) : (
+                    <Typography component={"span"}>
+                      End Date:{" "}
+                      {this.state.response.data.data.attributes.endDate}
+                    </Typography>
+                  )}
                 </Grid>
                 <Grid className={classes.cardSmall} item xs={2}>
                   <Typography>
@@ -159,6 +254,8 @@ class Details extends Component {
           </div>
         </main>
       );
+    } else {
+      return <div>hi</div>;
     }
   }
 }
