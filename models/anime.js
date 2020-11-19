41346fbd0806186385b324abed3141d1d53e30b3
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-11-19 09:40:10 -0500

    added Anime model

---
+++ models/anime.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AnimeSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  jpTitle: {
+    type: String,
+    require: true,
+  },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+  synopsis: {
+    type: String,
+    require: true,
+  },
+  startDate: {
+    type: Number,
+    require: true,
+  },
+  endDate: {
+    type: Number,
+    require: false,
+    default: "TBD",
+  },
+  avgRating: {
+    type: Number,
+    require: true,
+  },
+  rank: {
+    type: Number,
+    require: false,
+  },
+  ageRating: {
+    type: String,
+    require: false,
+  },
+  episodeCount: {
+    type: Number,
+    require: true,
+  },
+  lengthEpisode: {
+    type: Number,
+    require: false,
+  },
+});
+
+module.exports = Anime = mongoose.model("anime", AnimeSchema);
