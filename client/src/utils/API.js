9a7c7c3e3182c4b4bf18aa2101b4615b27b26e09
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-11-22 18:00:22 -0500

    added comments to API file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -22,22 +22,28 @@ export default {
       "https://kitsu.io/api/edge/" + drop + "?filter%5Btext%5D=" + query
     );
   },
+  // API call to get the information for the anime from the API call and adds those properties to the model
   saveAnime: function (animeData) {
     console.log(animeData);
     return axios.post("/api/animes", animeData);
   },
+  // grabs the anime's that are saved into the animes state
   getAnime: function (animes) {
     return axios.get("/api/animes", animes);
   },
+  // deletes an anime matching it's specific ID for the profile page
   deleteAnime: function (id) {
     return axios.delete("api/animes/" + id, id);
   },
+  // API call to get the information for the manga from the API call and adds those properties to the model
   saveManga: function (mangaData) {
     return axios.post("/api/mangas", mangaData);
   },
+  // grabs the manga's that are saved into the animes state
   getManga: function (mangas) {
     return axios.get("/api/mangas", mangas);
   },
+  // deletes a manga matching it's specific ID for the profile page
   deleteManga: function (id) {
     return axios.delete("api/mangas/" + id, id);
   },
