83f42b3a78eae4109a8a6114261dfdb2a8864185
frillic <69489498+crowe828@users.noreply.github.com>
2020-12-16 11:32:44 -0500

    working on resolving 404 on refresh for subdirectories

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(express.json());
 // serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
+  +app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // connect to Mongo DB
